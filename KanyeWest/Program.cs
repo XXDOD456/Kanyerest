57d24bee433913f1d5c7603622511543f058716e
XXDOD456 <kylemcconnell94@gmail.com>
2020-05-08 08:29:32 -0500

    finished API

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Dynamic;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kanyeURL = "https://api.kanye.rest";
+            var client = new HttpClient();
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine($"{kanyeQuote}");
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine($"Ronsponse: {ronQuote}");
+        }
+    }
+}
